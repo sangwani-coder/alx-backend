d95fb50c51ab563545a7aea3f2df3d9cf9394aa0
zyambo <mightypz2@gmail.com>
2022-07-17 11:40:24 +0000

    [FIX] typo in dict name

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -29,7 +29,7 @@ class LIFOCache(BaseCaching):
 
     def get(self, key):
         """ return value from self.cache_data linked to key"""
-        if key is None or key not in self.cache_date:
+        if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
 
