96d5077e54754a2efccef89f323ef2a749ad606b
zyambo <mightypz2@gmail.com>
2022-07-17 09:12:38 +0000

    [FIX] put pass statement in else close

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,9 +9,10 @@ class BasicCache(BaseCaching):
     """
     def put(self, key, item):
         """ assigns value for the key to a dictionary self.cache_data"""
-        if key is None or item is None:
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+        else:
             pass
-        self.cache_data[key] = item
 
     def get(self, key):
         """ returns the value in self.cache_data linked to key."""
