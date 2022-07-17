554ecbf22833131e328f4435a5f37e64f923e108
zyambo <mightypz2@gmail.com>
2022-07-17 09:17:54 +0000

    FIX] change or to and operator

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,10 +9,8 @@ class BasicCache(BaseCaching):
     """
     def put(self, key, item):
         """ assigns value for the key to a dictionary self.cache_data"""
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
-        else:
-            pass
 
     def get(self, key):
         """ returns the value in self.cache_data linked to key."""
