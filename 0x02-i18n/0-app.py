3560288deffbeb19618be49cf96490e4ec2b8d23
zyambo <mightypz2@gmail.com>
2022-07-19 10:36:46 +0200

    updatedocstring

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,9 +6,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
+    """
+    GET
+    return: 0-index.html
+    """
     return render_template('0-index.html')
 
 if __name__ == '__main__':
